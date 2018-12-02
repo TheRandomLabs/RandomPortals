00ff445b451d25d011a6eaebdc0cd273ff5e138f
TheRandomLabs <therandomlabsinc@gmail.com>
2018-12-02 14:35:52 +1100

    Register VEPTeleporter

--- src/main/java/com/therandomlabs/verticalendportals/CommonProxy.java
+++ src/main/java/com/therandomlabs/verticalendportals/CommonProxy.java
@@ -2,11 +2,13 @@ package com.therandomlabs.verticalendportals;
 
 import com.therandomlabs.verticalendportals.config.VEPConfig;
 import com.therandomlabs.verticalendportals.handler.EndPortalActivationHandler;
+import com.therandomlabs.verticalendportals.util.VEPTeleporter;
 import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
 	public void preInit() {
 		VEPConfig.reload();
+		VEPTeleporter.register();
 	}
 
 	public void init() {
