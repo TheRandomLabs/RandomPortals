408c310f4da073d7bac17acfa591829226d6d04e
TheRandomLabs <therandomlabsinc@gmail.com>
2020-06-03 15:59:33 +1000

    Topography compatibility fix attempt

--- src/main/java/com/therandomlabs/randomportals/block/RPOBlocks.java
+++ src/main/java/com/therandomlabs/randomportals/block/RPOBlocks.java
@@ -21,6 +21,7 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -82,7 +83,7 @@ public final class RPOBlocks {
 
 	private static ImmutableList<Block> blocksWithItems;
 
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOW)
 	public static void registerBlocks(RegistryEvent.Register<Block> event) {
 		Blocks.END_PORTAL_FRAME.setTranslationKey("endPortalFrameLateral");
 
